ca870f88c0387cef15741fceec78a5950f57ca49
Carl Egbert and Shradha Pulla <shradha@>
2016-06-14 11:46:56 -0700

    Add js code to make initial deposit work

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//Business Logic
+
+function Account (name,deposit) {
+  this.name = name;
+  this.balance = deposit;
+}
+
+Account.prototype.deposit = function (depositAmount) {
+  this.balance += depositAmount;
+}
+
+Account.prototype.withdrawal = function (withdrawalAmount) {
+  this.balance -= withdrawalAmount;
+}
+
+
+//User Interface Logic
+$(document).ready(function () {
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+
+    var nameInput = $("input#new-name").val();
+    var initialDepositInput = parseInt($("input#initial-deposit").val());
+
+    var newAccount = new Account (nameInput,initialDepositInput);
+    $("#account-info").show();
+    $("#account-balance").text(newAccount.balance);
+  });
+  $("#deposit-form").submit(function(event){
+    event.preventDefault();
+  });
+  $("#withdrawal-form").submit(function(event){
+    event.preventDefault();
+  });
+});
